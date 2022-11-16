dfb3a86405648819d15a10c6bd08dadbbd8991ff
Antonio Breyer <antoniofabiopbreyer@gmail.com>
2022-11-16 00:46:21 -0300

    feat: sign in token

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import express from "express"
 import cors from "cors"
 import { MongoClient } from "mongodb"
+import { v4 as uuidV4 } from "uuid"
 import Joi from "joi"
 import dotenv from "dotenv"
 import bcrypt from "bcrypt"
 
-const signInUserScheme = Joi.object({
+const signUpUserScheme = Joi.object({
     name: Joi.string().trim().min(3).required(),
-    email: Joi.string().trim().email(),
-    password: Joi.string().alphanum().trim().min(4).required()
+    email: Joi.string().trim().email().required(),
+    password: Joi.string().alphanum().trim().min(6).required()
 })
+
 
 dotenv.config()
 const app = express()
@@ -27,26 +29,50 @@ try {
 }
 
 const db = mongoClient.db("myWallet")
-const users = db.collection("users")
+const usersCollection = db.collection("users")
+const sessionsCollection = db.collection("sessions")
 
 
 app.post("/sign-up", async (req, res) => {
     const userInfo = req.body
     try {
-        const userExist = await users.findOne({ email: userInfo.email })
+        const userExist = await usersCollection.findOne({ email: userInfo.email })
         if (userExist) {
             return res.sendStatus(409)
         }
-        const validation = signInUserScheme.validate(userInfo, { abortEarly: false })
+        const validation = signUpUserScheme.validate(userInfo, { abortEarly: false })
         if (validation.error) {
             const errors = validation.error.details.map(d => d.message)
-            res.status(400).send({ message: errors })
+            return res.status(400).send({ message: errors })
         }
         const user = validation.value
-        const passwordHash = bcrypt.hashSync(user.password,10)
-        await users.insertOne({...user, password:passwordHash})
+        const passwordHash = bcrypt.hashSync(user.password, 10)
+        await usersCollection.insertOne({ ...user, password: passwordHash })
         res.send(201)
-    
+
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+
+})
+
+app.post("/sign-in", async (req, res) => {
+    const userInfo = req.body
+    try {
+        const user = await usersCollection.findOne({ email: userInfo.email })
+        const comparePassword = bcrypt.compareSync(userInfo.password, user.password)
+        if (!user || !comparePassword) {
+            return res.sendStatus(401)
+        }
+
+        const sessionExist = await sessionsCollection.findOne({ userId: user._id })
+        if (sessionExist) {
+            return res.send({ token: sessionExist.token })
+        }
+        const createToken = uuidV4()
+        await sessionsCollection.insertOne({ token: createToken, userId: user._id })
+        res.send({ token: createToken })
     } catch (err) {
         console.log(err)
         res.sendStatus(500)
