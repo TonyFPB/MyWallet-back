9313f29e7f47d348f45d92edbc4d61682259743c
Antonio Fabio <antoniofabiopbreyer@gmail.com>
2023-05-29 14:32:30 -0300

    test: sign in

--- tests/factories/user-factory.ts
+++ tests/factories/user-factory.ts
@@ -1,13 +1,18 @@
+import bcrypt from "bcrypt";
 import { faker } from "@faker-js/faker";
 import { prisma } from "../../src/database/db";
+import { User } from "@prisma/client";
 
 
-async function createUser(email?: string) {
+async function createUser(params: Partial<User> = {}) {
+  const password = params.password || faker.lorem.word(8);
+  const hashPassword = bcrypt.hashSync(password, 10);
+
   const user = await prisma.user.create({
     data: {
       name: faker.person.firstName(),
-      email: email || faker.internet.email(),
-      password: faker.lorem.word(8)
+      email: params.email || faker.internet.email(),
+      password: hashPassword
     }
   })
 
--- tests/integration/auth.test.ts
+++ tests/integration/auth.test.ts
@@ -12,7 +12,7 @@ beforeAll(async () => {
   cleanDb();
 })
 
-describe("POST /sign-up", () => {
+describe("POST /auth/sign-up", () => {
   it("should respond with status 400 when body does not exists", async () => {
     const response = await server.post("/auth/sign-up");
 
@@ -29,35 +29,96 @@ describe("POST /sign-up", () => {
 
     expect(response.status).toBe(httpStatus.BAD_REQUEST);
   })
-  it("should respond with status 409 when has a conflict with the email", async () => {
-    const userConflict = await userFactory.createUser();
-    const bodyUser = {
-      name: faker.person.firstName(),
-      email: userConflict.email,
-      password: faker.lorem.word(8)
-    };
 
-    const response = await server.post("/auth/sign-up").send(bodyUser);
+  describe("when body is valid",()=>{
+    const generateValidBody = (email?: string) => {
+      return {
+        name: faker.person.firstName(),
+        email: email || faker.internet.email(),
+        password: faker.lorem.word(6)
+      }
+    }
+    it("should respond with status 409 when has a conflict with the email", async () => {
+      const userConflict = await userFactory.createUser();
+      const bodyUser = generateValidBody(userConflict.email);
+  
+      const response = await server.post("/auth/sign-up").send(bodyUser);
+  
+      expect(response.status).toBe(httpStatus.CONFLICT);
+    })
 
-    expect(response.status).toBe(httpStatus.CONFLICT);
+    it("should resposnd with status 201 and create a user", async () => {
+      const bodyUser = generateValidBody();
+  
+      const response = await server.post("/auth/sign-up").send(bodyUser);
+      const newUser = await prisma.user.findUnique({ where: { email: bodyUser.email } });
+      expect(response.status).toBe(httpStatus.CREATED);
+      expect({
+        name: newUser?.name,
+        email: newUser?.email,
+      }).toEqual({
+        name: bodyUser.name,
+        email: bodyUser.email
+      });
+    })
   })
-  it("should resposnd with status 201 and create a user", async () => {
-    const bodyUser = {
-      name: faker.person.firstName(),
-      email: faker.internet.email(),
-      password: faker.lorem.word(8)
+
+
+})
+
+describe("POST /auth/sign-in", () => {
+  it("should respond with status 400 when body does not exists", async () => {
+    const response = await server.post("/auth/sign-in");
+
+    expect(response.status).toBe(httpStatus.BAD_REQUEST);
+  })
+
+  it("should respond with status 400 when is a invalid body", async () => {
+    const invalidBody = {
+      email: faker.person.firstName(),
+      password: faker.number.int()
     };
 
-    const response = await server.post("/auth/sign-up").send(bodyUser);
-    const newUser = await prisma.user.findUnique({ where: { email: bodyUser.email } });
-    expect(response.status).toBe(httpStatus.CREATED);
-    expect({
-      name: newUser?.name,
-      email: newUser?.email,
-    }).toEqual({
-      name: bodyUser.name,
-      email: bodyUser.email
-    });
+    const response = await server.post("/auth/sign-in").send(invalidBody);
+
+    expect(response.status).toBe(httpStatus.BAD_REQUEST);
+  })
+
+  it("should respond with status 401 when user credentials are wrong", async () => {
+    const user = await userFactory.createUser();
+    const body = { email: user.email, password: faker.lorem.word(6) };
+
+    const response = await server.post("/auth/sign-in").send(body);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  })
+
+  describe("when credentials are valid", () => {
+    const generateValidBody = () => {
+      return {
+        email: faker.internet.email(),
+        password: faker.lorem.word(6)
+      }
+    }
+
+    it("should respond with status 200 and return the token user", async () => {
+      const body = generateValidBody();
+      const user = await userFactory.createUser(body);
+
+
+      const response = await server.post("/auth/sign-in").send(body);
+
+      const userSession = await prisma.user.findUnique({
+        where: { email: user.email },
+        include: { Session: true }
+      });
+
+      expect(response.status).toBe(httpStatus.OK);
+      expect(response.body).toEqual({
+        user: userSession?.name,
+        token: userSession?.Session?.token
+      });
+    })
   })
 
 })
